e0e59f325d24cc766113a4b06ff6bdc14a161d22
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-06 15:50:30 +0100

    Added up and down functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,4 +12,14 @@ describe('Thermostat', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
+  it('increases in temperature with up()', () => {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
 });

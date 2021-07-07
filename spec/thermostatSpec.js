664bcdd02588ac8596d7a596d2cb4c1ac37cfa43
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-07 11:42:46 +0100

    Added limit to PSM

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -22,4 +22,36 @@ describe('Thermostat', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 
+  it('has a minimum of 10 degrees', () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10)
+  });
+
+  it('it has power saving mode on by default', () => {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true)
+  });
+
+  it('can switch PSM off', () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch PSM back on', () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  describe('when power saving mode is on', () => {
+    it('has a maximum temperature of 25 degrees', () => {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,15 +2,44 @@
 
 class Thermostat{
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
   }
   getCurrentTemperature() {
     return this.temperature;
   }
  up() {
-   this.temperature += 1
+   if (this.isMaximumTemperature()) {
+     return;
+   }
+   this.temperature += 1;
  }
  down() {
+   if (this.isMinimumTemperature()) {
+     return;
+   }
    this.temperature -= 1
  }
+ isMinimumTemperature() {
+   return this.temperature === this.MINIMUM_TEMPERATURE;
+ }
+ isMaximumTemperature() {
+   if(this.switchPowerSavingModeOn() === false) {
+     return this.temperature === this.MAX_LIMIT_PSM_OFF;
+   }
+   return this.temperature === this.MAX_LIMIT_PSM_ON
+ }
+ isPowerSavingModeOn() {
+   return this.powerSavingMode;
+ }
+ switchPowerSavingModeOff() {
+   this.powerSavingMode = false;
+ }
+ switchPowerSavingModeOn() {
+   this.powerSavingMode = true;
+ }
+
 };
